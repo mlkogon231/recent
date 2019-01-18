ed3ac0ed2921155d89ec69616e70fd5c12332efe
Mark Kogon <mlkogon231@gmail.com>
2019-01-17 19:17:46 -0800

    First commit of recent stuff

---
+++ WiredBrainCoffee/Pages/Contact.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WiredBrainCoffee.Models;
+using WiredBrainCoffee.Services;
+
+namespace WiredBrainCoffee.Pages
+{
+    public class ContactModel : PageModel
+    {
+        [BindProperty]
+        public Contact Contact { get; set; }
+        public string Message { get; private set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                EmailService.SendEmail(Contact);
+                return RedirectToPage("Confirmation", "Contact");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+
+        public IActionResult OnPostSubscribe(string address)
+        {
+            EmailService.SendEmail(address);
+            return RedirectToPage("Confirmation", "Subscribe");
+        }
+    }
+}
---
+++ WiredBrainCoffee/Pages/Menu.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using WiredBrainCoffee.Models;
+using WiredBrainCoffee.Services;
+
+namespace WiredBrainCoffee.Pages
+{
+    public class MenuModel : PageModel
+    {
+        private IMenuService menuService;
+        private ILogger<MenuModel> logger;
+
+        public List<MenuItem> Menu { get; set; }
+
+        public MenuModel(IMenuService menuService, ILogger<MenuModel> logger)
+        {
+            this.menuService = menuService;
+            this.logger = logger;
+        }
+
+        public void OnGet()
+        {
+            try
+            {
+                Menu = menuService.GetMenuItems();
+                throw new Exception();
+            }
+            catch
+            {
+                logger.Log(LogLevel.Debug, "Could not retrieve menu");
+            }
+            
+        }
+    }
+}
